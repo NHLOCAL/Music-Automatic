d01a2ad98ff78b40c4428a4117e550e5a63c592e
NHLOCAL <NH.LOCAL11@GMAIL.COM>
2024-02-03 22:59:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import os
 
-
+from jibrish_to_hebrew import jibrish_to_hebrew
+import eyed3
 
 
 class FileManager:
     def __init__(self, root_dir):
         self.root_dir = root_dir
 
+    def perform_action(self, action):
+        if action == 1:
+            self.delete_empty_folders()
+        elif action == 2:
+            self.fix_jibrish_files()
 
 
     def build_folder_structure(self, func):
@@ -15,6 +21,9 @@ class FileManager:
     
 
     def delete_empty_folders(self):
+    
+        '''מחיקת תיקיות ריקות'''
+        
         delete_folders = []
 
         # בניית רשימת התיקיות למחיקה
@@ -27,10 +36,43 @@ class FileManager:
 
         print(f'Num. of empty folders deleted: {len(delete_folders)}')
 
-    def perform_action(self, action):
-        if action == 1:
-            self.delete_empty_folders()
-        # Add more actions as needed
+
+
+
+    def fix_jibrish_files(self):
+    
+        '''המרת קבצים עם קידוד פגום לעברית תקינה'''
+        
+        for root, dirs, files in os.walk(self.root_dir):
+            for file in files:
+                if file.lower().endswith((".mp3", ".wav", ".wma")):
+                    file_path = os.path.join(root, file)
+
+                    try:
+                        # Load the MP3 file
+                        audiofile = eyed3.load(file_path)
+
+                        # Apply custom function to album name and title
+                        if audiofile.tag.album:
+                            new_album_name = jibrish_to_hebrew(audiofile.tag.album)
+                            audiofile.tag.album = new_album_name
+
+                        if audiofile.tag.title:
+                            new_title = jibrish_to_hebrew(audiofile.tag.title)
+                            audiofile.tag.title = new_title
+
+                        # Save changes to the MP3 file
+                        audiofile.tag.save()
+
+                        print(f"Updated Album: {new_album_name}")
+                        print(f"Updated Title: {new_title}")
+
+                    except Exception as e:
+                        print(f"Error processing {file}: {e}")
+
+                    print("")
+
+
 
 if __name__ == "__main__":
     root_directory = input('Add path folder\n>>>')
