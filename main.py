566c323939263a9b36653a1b8098d1d89d25c952
NHLOCAL <NH.LOCAL11@GMAIL.COM>
2024-02-03 22:41:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import os
 
+
+
+
 class FileManager:
     def __init__(self, root_dir):
         self.root_dir = root_dir
 
+
+
+    def build_folder_structure(self, func):
+        """יצירת רשימת קבצים ותיקיות"""
+        pass
+    
+
     def delete_empty_folders(self):
         delete_folders = []
 
+        # בניית רשימת התיקיות למחיקה
         for folder_name, subfolders, filenames in os.walk(self.root_dir, topdown=False):
             for subfolder in subfolders:
                 folder_path = os.path.join(folder_name, subfolder)
@@ -17,7 +28,7 @@ class FileManager:
         print(f'Num. of empty folders deleted: {len(delete_folders)}')
 
     def perform_action(self, action):
-        if action == "delete_empty_folders":
+        if action == 1:
             self.delete_empty_folders()
         # Add more actions as needed
 
@@ -25,5 +36,5 @@ if __name__ == "__main__":
     root_directory = input('Add path folder\n>>>')
     file_manager = FileManager(root_directory)
 
-    action = input('Choose action (delete_empty_folders, etc.)\n>>>')
-    file_manager.perform_action(action)
+    action = input('Choose action ([1] delete_empty_folders, [2] etc.)\n>>>')
+    file_manager.perform_action(int(action))
