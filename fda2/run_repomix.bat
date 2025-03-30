b6a0dc38c481657d7bc1640f74d1b2a94dd5b49d
NHLOCAL <NH.LOCAL11@GMAIL.COM>
2025-03-30 03:43:55 +0300

    Update run_repomix.bat

--- fda2/run_repomix.bat
+++ fda2/run_repomix.bat
@@ -1,3 +1,4 @@
 @echo off
 
-npx repomix C:\Users\me\Documents\GitHub\Music-Automatic\fda2  -i "data/**,logs/**"
+del repomix-output.xml
+npx repomix C:\Users\me\Documents\GitHub\Music-Automatic\fda2  -i "data/**,logs/**,run_repomix.bat,experiments/**"
