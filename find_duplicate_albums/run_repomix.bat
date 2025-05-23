0640fe7cd57b1724235bca0b3e0ec601632ac062
NHLOCAL <NH.LOCAL11@GMAIL.COM>
2025-05-23 04:51:09 +0300

    Update run_repomix.bat

--- find_duplicate_albums/run_repomix.bat
+++ find_duplicate_albums/run_repomix.bat
@@ -1,3 +1,3 @@
 @echo off
 
-npx repomix -i "data/**,logs/**,run_repomix.bat,experiments/**,repomix-output.*" --style markdown --remove-comments
+npx repomix -i "data/**,logs/**,run_repomix.bat,experiments/**,repomix-output.*,app.py" --style markdown --remove-comments
