5630144a014ba4e1a45de26ea974edd157986908
NHLOCAL <NH.LOCAL11@GMAIL.COM>
2026-03-08 05:59:35 +0200

    Update run-repomix.bat

--- album_deduplicator/run-repomix.bat
+++ album_deduplicator/run-repomix.bat
@@ -1,3 +1,3 @@
 @echo off
 
-npx repomix -i "data/**,logs/**,run-repomix.bat,experiments/**,repomix-output.*,app.py,create_ml_dataset.py,similarity_model/**,legacy/**,Notes.txt,docs/**,tests/**,**/frontend/package-lock.json," --style markdown --remove-comments --remove-empty-lines --include "docs/ARCHITECTURE.md"
+npx repomix -i "data/**,logs/**,run-repomix.bat,experiments/**,repomix-output.*,app.py,create_ml_dataset.py,similarity_model/**,legacy/**,Notes.txt,docs/**,tests/**,**/frontend/package-lock.json," --style markdown --remove-comments --remove-empty-lines
